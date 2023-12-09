45d696d9f55ad0f15ffd0feced2b11dd99aa5105
mahdi <mahdimamashli1383@gmail.com>
2023-12-09 12:46:50 +0330

    handel errr 404

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express')
 const bodyParser = require("body-parser")
 const app = express()
 const path = require("path")
+const controllersErr = require('./controllers/error.js')
 app.set("view engine" , "ejs")
 app.set("views", "views")
 app.use(bodyParser.urlencoded({extended : false}))
 app.use(express.static(path.join(__dirname, 'public')))
 
-
+app.use(controllersErr.err404)
 
 
 app.listen(3000, ()=>{
---
+++ controllers/error.js
@@ -0,0 +1,3 @@
+exports.err404 = (req,res)=>{
+    res.status(404).render('404', {title: "Page Not Found-404"})
+}
