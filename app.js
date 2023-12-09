d31d95817dfd47df3aea9254f104a5fe1d8e7e87
mahdi <mahdimamashli1383@gmail.com>
2023-12-09 12:29:32 +0330

    create all pages

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const bodyParser = require("body-parser")
 const app = express()
 
 
+app.engine("view engine" , "ejs")
+app.set("views", "views")
+app.use(bodyParser.urlencoded({extended : false}))
 
+app.use((req,res)=>{
+})
 app.listen(3000, ()=>{
     console.log('run server on port 3000');
 })
